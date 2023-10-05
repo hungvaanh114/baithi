46cb85270660d15351050a864b27619f4d712566
hungvaanh114 <hung841200@gmail.com>
2023-10-06 06:39:36 +0700

    update

--- MVC/Views/HomeView.php
+++ MVC/Views/HomeView.php
@@ -9,7 +9,7 @@
 	<div class="conteiner" style="background-color: #e5e4e4; ">
 		<div style="text-align: center;">
 			<h2 style="color: blue">
-				CHƯƠNG TRÌNH QUẢN LÝ SÁCH</h2>
+				CHƯƠNG TRÌNH QUẢN LÝ Cà PHÊ</h2>
 		</div>
 		<div style="text-align: center">
 			<img src="http://localhost/QLS_MVC_API/MVC/Public/images/anhnensach.jpg " style="width: 80%;
